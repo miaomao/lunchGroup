75e65b0ed44eab862d7dbec14579604b7ed18c92
miaomao <miss.liumiao@gmail.com>
2013-01-27 23:01:36 +0800

    initial dump

---
+++ data/dump_01272013.sql
@@ -0,0 +1,92 @@
+-- MySQL dump 10.13  Distrib 5.5.19, for Win32 (x86)
+--
+-- Host: localhost    Database: lunch
+-- ------------------------------------------------------
+-- Server version	5.5.19
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `expense_items`
+--
+
+DROP TABLE IF EXISTS `expense_items`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `expense_items` (
+  `eType_id` int(3) NOT NULL,
+  `account_id` int(3) NOT NULL,
+  `created` date NOT NULL DEFAULT '0000-00-00',
+  `value` float DEFAULT NULL,
+  PRIMARY KEY (`eType_id`,`account_id`,`created`),
+  KEY `fk_eitem_user` (`account_id`),
+  CONSTRAINT `fk_eitem_etype` FOREIGN KEY (`eType_id`) REFERENCES `expense_types` (`id`),
+  CONSTRAINT `fk_eitem_user` FOREIGN KEY (`account_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `expense_items`
+--
+
+LOCK TABLES `expense_items` WRITE;
+/*!40000 ALTER TABLE `expense_items` DISABLE KEYS */;
+/*!40000 ALTER TABLE `expense_items` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `expense_types`
+--
+
+DROP TABLE IF EXISTS `expense_types`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `expense_types` (
+  `id` int(3) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  `attr` enum('+','-') NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(3) NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(10) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `leftover` float DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2013-01-26 16:21:20
